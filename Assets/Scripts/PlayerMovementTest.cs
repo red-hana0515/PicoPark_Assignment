54ca9674a83643ecd5e47407e67d4901f6c6ef34
Ewan-e <85921972+Ewan-e@users.noreply.github.com>
2023-04-08 07:48:11 +0800

    Moving Works but connecting doesnt??

---
+++ Assets/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Mirror;
+
+public class PlayerMovementTest : NetworkBehaviour
+{
+    private Camera mainCamera;
+
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    #region server
+    [Command]
+    private void CmdMove(Vector2 position)
+    {
+        rb.velocity = position;
+    }
+
+    private void CmdJump()
+    {
+        //if (Input.GetButtonDown("Jump") && IsGrounded())
+        //{
+        //   rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        //}
+
+        if (Input.GetButtonDown("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    [Command]
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    //[Command]
+    //private bool IsGrounded()
+    //
+    //    return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    //}
+
+    #endregion
+
+    #region client
+
+    //start method for the client who owns the object
+    public override void OnStartAuthority()
+    {
+        mainCamera = Camera.main;
+    }
+
+    [ClientCallback] //created as a client only update function (all clients will call it not the server)
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        CmdJump();
+        Flip();
+    }
+
+    [ClientCallback]
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(horizontal * speed, rb.velocity.y);
+        CmdMove(movement);
+    }
+
+    #endregion
+}
+
+
+
